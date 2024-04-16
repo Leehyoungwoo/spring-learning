035e76d44fccfc59f1dc5b103e65242c288dac37
Leehyoungwoo <dlguddn3024@gmail.com>
2024-04-16 12:57:08 +0900

    refactor: fix key to message

--- src/main/java/com/springlearning/global/security/handler/LoginAuthenticationFailureHandler.java
+++ src/main/java/com/springlearning/global/security/handler/LoginAuthenticationFailureHandler.java
@@ -10,6 +10,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.AuthenticationCredentialsNotFoundException;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.InternalAuthenticationServiceException;
+import org.springframework.security.authentication.LockedException;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
@@ -53,10 +54,10 @@ public class LoginAuthenticationFailureHandler implements AuthenticationFailureH
             errorMessage.append("존재하지 않는 계정입니다. 회원가입 후 로그인해주세요.");
         } else if (exception instanceof AuthenticationCredentialsNotFoundException) {
             errorMessage.append("인증 요청이 거부되었습니다. 관리자에게 문의하세요.");
-        } else {
-            errorMessage.append("알 수 없는 오류로 로그인 요청을 처리할 수 없습니다. 관리자에게 문의하세요.");
+        } else if (exception instanceof LockedException){
+            errorMessage.append(exception.getMessage());
         }
-        responseMap.put("errorMessage", errorMessage.toString());
+        responseMap.put("message", errorMessage.toString());
 
         response.setCharacterEncoding("UTF-8");
         response.setContentType("application/json");
