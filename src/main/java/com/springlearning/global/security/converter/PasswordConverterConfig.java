cdd6457be41158b223cc3e4c1db215ba7054c0e2
dlguddn3024 <dlguddn3024@gmail.com>
2024-04-17 01:20:31 +0900

    fix: passwordConverter return way

--- src/main/java/com/springlearning/global/security/converter/PasswordConverterConfig.java
+++ src/main/java/com/springlearning/global/security/converter/PasswordConverterConfig.java
@@ -2,7 +2,7 @@ package com.springlearning.global.security.converter;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
@@ -10,6 +10,6 @@ public class PasswordConverterConfig {
 
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 }
