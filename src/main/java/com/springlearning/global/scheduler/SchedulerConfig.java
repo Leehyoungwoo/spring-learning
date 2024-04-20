82905ac390e29b0b19b85d72a282ccdb1066673c
dlguddn3024 <dlguddn3024@gmail.com>
2024-04-20 20:46:51 +0900

    feat: add implement spring batch & schedLock

---
+++ src/main/java/com/springlearning/global/scheduler/SchedulerConfig.java
@@ -0,0 +1,18 @@
+package com.springlearning.global.scheduler;
+
+import net.javacrumbs.shedlock.core.LockProvider;
+import net.javacrumbs.shedlock.provider.redis.spring.RedisLockProvider;
+import net.javacrumbs.shedlock.spring.annotation.EnableSchedulerLock;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+
+@Configuration
+@EnableSchedulerLock(defaultLockAtMostFor = "PT10M", defaultLockAtLeastFor = "PT30M")
+public class SchedulerConfig {
+
+    @Bean
+    public LockProvider lockProvider(RedisConnectionFactory connectionFactory) {
+        return new RedisLockProvider(connectionFactory);
+    }
+}
